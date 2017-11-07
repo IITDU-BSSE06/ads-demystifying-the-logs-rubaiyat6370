5d405612f2707ec0fe3319e3d10dc31b5a6f7433
Rubaiyat Jahan Mumu <rubaiyat6370@users.noreply.github.com>
2017-11-07 22:44:39 +0600

    reducer.py is completed for assignment 2

--- assignment2/reducer.py
+++ assignment2/reducer.py
@@ -3,11 +3,11 @@
 import sys
 
 hit = 0
-
 for line in sys.stdin:
 	data = line.strip()
+	if data == "" or data is None:
+		continue
 	url = data
 	if "/assets/js/the-associates.js" == url:
 		hit +=1
-
 print hit
