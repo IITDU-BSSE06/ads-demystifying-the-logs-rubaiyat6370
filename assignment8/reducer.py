f88de565badd9a55ae7631d0f55f2914e5801b0d
Rubaiyat Jahan Mumu <rubaiyat6370@users.noreply.github.com>
2017-11-08 12:59:46 +0600

    done

---
+++ assignment8/reducer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import sys
+hit = 0
+oldurl = None
+maxCount = -9999
+
+for line in sys.stdin:
+	data = line.strip()
+	if data == "" or data is None:
+		continue
+	url = data
+	if oldurl and oldurl != url:
+		#print oldurl, "\t", hit
+		if hit > maxCount:
+			path = oldurl
+			maxCount = hit
+		oldurl = url
+		hit = 0
+	oldurl = url
+	hit += 1
+print path, "\t", maxCount
+	
